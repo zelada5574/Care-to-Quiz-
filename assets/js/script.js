004c26f6c2a601fda9d8023390ddba91e1f4bcee
Jaryot <jarram999@gmail.com>
2023-03-05 21:02:34 -0800

    added questions to Js file for quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,104 @@
-var timeleft = document.querySelector("#timeleft");
-var startButton = document.querySelector("#start");
+const Questions= [
+
+{
+  prompt: "Commonly used data types DO NOT include?\n (a) strings\n (b) boolean\n (c) number\n (d) lens",
+
+  answers: "d"
+
+},
+
+{
+  prompt: "The condition of an if/else statement is enclosed with?\n (a) brackets\n (b) elipses\n (c) parenthesis\n (d) hugs",
+
+  answers: "c"
+
+},
+
+{
+  prompt: "What does 'return' do in a fucntion?\n (a) nothing\n (b) runs the function again\n (c) voids entire function\n (d) returns specified value from function",
+
+  answers: "d"
+
+},
+
+{
+  prompt: "Which of the following is true about variable naming conventions in JavaScript?\n (a) variables must begin with a letter or underscore character\n (b) variables are case sensitive\n (c) Both a & b\n (d) Neither a or b",
+
+  answers: "c"
+
+},
+
+{
+  prompt: "Commonly used data types DO NOT include?\n (a) strings\n (b) boolean\n (c) number\n (d) lens",
+
+  answers: "d"
+
+},
+
+{
+  prompt: "Which of the follwing type of variable is only visible in it's defined function?\n (a) global\n (b) lcoal\n (c) Both a & b\n (d) Neither a or b",
+
+  answers: "b"
+
+},
+
+{
+  prompt: "Which function gives the length of a variable?\n (a) .total\n (b) .len\n (c) .length\n (d) .space",
+
+  answers: "c"
+
+},
+
+{
+  prompt: "Which function returns an integer of a string argument?\n (a) parseInt()\n (b) toString()\n (c) function()\n (d) toFixed()",
+
+  answers: "a"
+
+},
+
+{
+  prompt: "Which keyword is used to define a variable in Javascript?\n (a) var\n (b) let\n (c) Both a&b\n (d) Neither a or b",
+
+  answers: "c"
+
+},
+
+{
+  prompt: "Which of the following methods is used to access HTML IDs using Javascript?\n (a) getElementbyI()\n (b) getElementbyClassName()\n (c) Both a & b\n (d) Neither a or b",
+
+  answers: "c"
+
+},
+
+{
+  prompt: "How can a datatype be declard a constant type?\n (a) const\n (b) var\n (c) let\n (d) constant",
+
+  answers: "a"
+
+},
+
+{
+  prompt: "JavaSCript is an ____ language?\n (a) Object-Oriented\n (b) Object-Based\n (c) Procedural\n (d) None of the above",
+
+  answers: "a"
+
+},
+
+
+
+
+
+
+
+
+
+
+
+
+]
+
+let timeleft = document.querySelector("#timeleft");
+let startButton = document.querySelector("#start");
 
 
 startButton.addEventListener("click", function() {
